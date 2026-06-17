d91c613ed10d1db758294a5cb6659d2c46301f0b
sneha2427 <1dt24cs277@dsatm.edu.in>
2026-06-17 21:54:36 +0530

    Added JS AND CSS

---
+++ announcment.js
@@ -0,0 +1,9 @@
+let announcements = [
+"Food Distribution Camp on 20 April",
+"Solar Energy Workshop on 5 May",
+"Tree Plantation Drive on 15 May"
+];
+
+for(let i=0;i<announcements.length;i++){
+    console.log(announcements[i]);
+}
