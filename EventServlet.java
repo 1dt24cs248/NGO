0563cee07f7ac52528f794b43db239decb81efb6
1dt24cs248 <1dt24cs248@dsatm.edu.in>
2026-07-13 14:40:56 +0530

    Event section Added

---
+++ EventServlet.java
@@ -0,0 +1,41 @@
+package com.ngo.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import com.ngo.util.DBConnection;
+
+@WebServlet("/event")
+public class EventServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+
+        try {
+            String name = req.getParameter("name");
+            String date = req.getParameter("date");
+            String location = req.getParameter("location");
+
+            Connection con = DBConnection.getConnection();
+
+            PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO events(name, date, location) VALUES(?,?,?)"
+            );
+
+            ps.setString(1, name);
+            ps.setString(2, date);
+            ps.setString(3, location);
+
+            ps.executeUpdate();
+
+            res.sendRedirect(req.getContextPath() + "/events.jsp?success=1");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
