cb628e2efc1ba150fa7c9f25c9bb985ad94216d5
sneha2427 <1dt24cs277@dsatm.edu.in>
2026-07-13 16:11:16 +0530

    initial commit

--- helprequest.java
+++ helprequest.java
@@ -41,3 +41,40 @@ public class HelpRequestServlet extends HttpServlet {
         }
     }
 }
+
+package com.ngo.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import com.ngo.util.DBConnection;
+
+@WebServlet("/helpRequest")
+public class HelpRequestServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+
+        try {
+            Connection con = DBConnection.getConnection();
+
+            PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO help_requests(name, location, need, mobile) VALUES(?,?,?,?)"
+            );
+
+            ps.setString(1, req.getParameter("name"));
+            ps.setString(2, req.getParameter("location"));
+            ps.setString(3, req.getParameter("need"));
+            ps.setString(4, req.getParameter("mobile"));
+
+            ps.executeUpdate();
+
+            res.sendRedirect("helprequest.jsp?success=1");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
