f779a2dbd6c1c90ff579a8090dc4cf614d1e4b34
sneha2427 <1dt24cs277@dsatm.edu.in>
2026-07-13 16:21:21 +0530

    initial commit

--- FeedbackServlet.java
+++ FeedbackServlet.java
@@ -43,3 +43,43 @@ public class FeedbackServlet extends HttpServlet {
         }
     }
 }
+
+
+package com.ngo.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import com.ngo.util.DBConnection;
+
+@WebServlet("/feedback")
+public class FeedbackServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+
+        try {
+
+            Connection con = DBConnection.getConnection();
+
+            PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO feedback(name, message) VALUES(?, ?)"
+            );
+
+            ps.setString(1, req.getParameter("name"));
+            ps.setString(2, req.getParameter("message"));
+
+            ps.executeUpdate();
+
+            res.sendRedirect("feedback.jsp?success=1");
+
+        } catch (Exception e) {
+            System.out.println(e);
+            res.sendRedirect("feedback.jsp?error=1");
+        }
+    }
+}
